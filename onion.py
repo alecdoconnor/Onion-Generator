9439e947b6a10d53c83dafd09e8a827be9242d43
Alec O'Connor <alecdoconnor@users.noreply.github.com>
2017-08-09 20:41:27 -0500

    Add files via upload
    Note that words come from an online 5000+- most popular English word list to get it started. `words.txt` should list each work on a single line, and include only letters `A-Z` and numbers `2-7`.

---
+++ onion.py
@@ -0,0 +1,48 @@
+import sys,hashlib,base64
+from Crypto.PublicKey import RSA
+
+onion = "yvuqbyepwaowxm2p"
+search = ["alpha", "beta", "gamma", "delta", "epsilon", "zeta"]
+# search = [line.rstrip('\n') for line in open('words.txt')]
+count = 0
+
+
+def searchIn(onion):
+    results = ""
+    rating = 0
+    for value in search:
+        if len(value) >= 4 and onion.startswith(value):
+            results = results+"-"+value
+            rating += (pow(len(value), 2) * 2)
+        elif len(value) >= 3 and onion.find(value) != -1:
+            results = results+"-"+value
+            rating += pow(len(value), 2)
+    if (any(str.isdigit(c) for c in onion) == False):
+        rating += 10
+    if results == "":
+        return 0, 0
+    else:
+        return results[1:], rating
+
+while 1:
+    searchResult = 0
+    while searchResult == 0:
+        key = RSA.generate(1024)
+
+        priv = key.exportKey('PEM')
+        pub = key.publickey().exportKey('PEM')
+        onion = base64.b32encode(hashlib.sha1(pub).digest()[:10]).lower()
+        searchResult, searchRating = searchIn(onion)
+        count = count + 1
+    print "Rating:", searchRating, searchResult, onion, "#", count
+
+    f = open('keys/%s-%s-%s.pem' % (searchRating, searchResult, onion), 'w')
+    f.write(priv)
+    f.close()
+    
+    f = open('keys/%s-%s-%s.pub' % (searchRating, searchResult, onion), 'w')
+    f.write(pub)
+    f.close()
+    
+    onion = "1111111111111111"
+    searchRating = 0
